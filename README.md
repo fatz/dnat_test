f459d382488e986218592eeac7f1afdafb3f0f4b
Jan Ulferts <julferts@mesosphere.com>
2019-06-12 15:26:00 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# dnat_issue
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: dnat_issue
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
